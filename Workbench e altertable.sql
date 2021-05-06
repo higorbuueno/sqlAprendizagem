72041c7d0872ab274a4758219d049b45b78e47de
higorbuueno <buenoshigor@gmail.com>
2021-05-05 22:42:53 -0300

    Update Workbench e altertable.sql

--- Workbench e altertable.sql
+++ Workbench e altertable.sql
@@ -11,3 +11,25 @@ alter table aluno drop column idadeAluno;
 alter table aluno modify column nomeAluno varchar (80) not NULL;
 
 alter table aluno change column nomeAluno nome_aluno varchar(30);
+
+-- Agora vamos para os exemplos --
+drop table aluno;
+
+create table professor
+(
+	cpf int primary key,
+    nomeProfessor varchar(50),
+    idade int
+);
+
+create table aluno
+(
+	 idAluno int primary key auto_increment,
+     nomeAluno varchar(50) not null,
+     idade int not null
+);
+
+alter table aluno add fk_professor_cpf int;
+
+alter table aluno add foreign key(fk_professor_cpf) references professor (cpf);
+
